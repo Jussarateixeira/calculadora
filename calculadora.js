03e40ed78756064ae077f921ec6c7ddaf0eb819d
Jussara Teixeira <jussara.teixeira.goncalves2@gmail.com>
2024-04-23 22:30:00 -0300

    Atualização do do projeto

---
+++ calculadora.js
@@ -0,0 +1,24 @@
+const display = document.querySelector('#display');
+const buttons = document.querySelectorAll('input[type=button]');
+
+buttons.forEach((myButton) => {
+
+    myButton.onclick = () => {
+
+        if (myButton.value == 'AC') {
+            display.value = '';
+        } else if (myButton.value == 'DE') {
+            let myValue = display.value;
+            display.value = myValue.toString().slice(0, -1);
+        } else if (myButton.value == '=' && display.value == '') {
+            alert('Você não digitou nenhum número!');
+        } else if (myButton.value == '=') {
+            display.value = eval(display.value);
+        } else {
+            display.value += myButton.value;
+        }
+
+    }
+
+})
+
